3fe277b87ec63f9bbefb25aee9d168b0175619a0
Alex Kovalenko <alex.kov.job@gmail.com>
2021-01-06 13:41:29 +0200

    add setup script for vim

---
+++ setup_vim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install vim
+
+ln -sfv $(pwd)/vimrc $HOME/.vimrc
+
+sudo apt install fzf
+sudo apt install ripgrep
+sudo apt install universal-ctags
+sudo apt install silversearcher-ag
+sudo apt install fd-find
