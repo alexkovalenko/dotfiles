5dfac6025b353cff0201631047b0e9f7748ce6db
Alex Kovalenko <alex.kov.job@gmail.com>
2021-01-17 18:14:25 +0200

    change file structure; change vim config; add i3 config

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#install stuff
 sudo apt install --yes vim
-
-ln -sfv $(pwd)/vimrc $HOME/.vimrc
-
 sudo apt install --yes fzf
 sudo apt install --yes ripgrep
 sudo apt install --yes universal-ctags
 sudo apt install --yes silversearcher-ag
 sudo apt install --yes fd-find
+
+#add links to configs
+ln -sfv $(pwd)/vim/vimrc $HOME/.vimrc
+mkdir -p $HOME/.i3 && ln -sfv $(pwd)/i3/config $HOME/.i3/config
+
